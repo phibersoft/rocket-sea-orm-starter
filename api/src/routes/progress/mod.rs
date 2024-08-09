9781f0e7392d4076cd6ac64825cd5f16546b8884
phibersoft <spirnet@hotmail.com>
2024-08-09 04:22:07 +0300

    fix: All routes should return JsonResponse

--- api/src/routes/progress/mod.rs
+++ api/src/routes/progress/mod.rs
@@ -5,11 +5,13 @@ use db::tables::progress::Model;
 use rocket::{get, Route, routes};
 use rocket::serde::json::Json;
 
+use crate::types::JsonResponse;
+
 #[get("/")]
-async fn list(conn: Connection<pool::Db>) -> Json<Vec<Model>> {
+async fn list(conn: Connection<pool::Db>) -> JsonResponse<Vec<Model>> {
     let db = conn.into_inner();
     let rows = Progress::find().all(&db).await.unwrap();
-    Json(rows)
+    Ok(Json(rows))
 }
 
 pub fn routes() -> Vec<Route> {
--- api/src/routes/task/mod.rs
+++ api/src/routes/task/mod.rs
@@ -4,21 +4,23 @@ use db::tables::{Task, task};
 use rocket::{get, post, Route, routes};
 use rocket::serde::json::Json;
 
+use crate::types::JsonResponse;
+
 #[get("/")]
-async fn list(conn: Connection<pool::Db>) -> Json<Vec<task::Model>> {
+async fn list(conn: Connection<pool::Db>) -> JsonResponse<Vec<task::Model>> {
     let db = conn.into_inner();
     let rows = Task::find().all(&db).await.unwrap();
-    Json(rows)
+    Ok(Json(rows))
 }
 
 #[post("/", format = "json", data = "<input_data>")]
-async fn new(conn: Connection<pool::Db>, input_data: Json<task::InputData>) -> Json<task::Model> {
+async fn new(conn: Connection<pool::Db>, input_data: Json<task::InputData>) -> JsonResponse<task::Model> {
     let data = input_data.into_inner();
 
     let task = task::Model::new(data);
     let task: task::Model = task.insert(&conn.into_inner()).await.unwrap();
 
-    Json(task)
+    Ok(Json(task))
 }
 
 pub fn routes() -> Vec<Route> {
