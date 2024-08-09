3c2b443ba47fa0fd671d3d2bfca9fe691cde3d17
phibersoft <spirnet@hotmail.com>
2024-08-09 04:21:51 +0300

    feat: better error catching

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,12 +2,26 @@ use db::{Database, run_migrations};
 use db::pool;
 use rocket::{Build, catch, catchers, Request, Rocket};
 use rocket::fairing::AdHoc;
+use rocket::http::Status;
+
+use crate::types::ErrorResponse;
 
 mod routes;
+mod types;
 
 #[catch(404)]
-fn not_found(req: &Request) -> String {
-    format!("Sorry, '{}' is not a valid path.", req.uri())
+fn not_found() -> ErrorResponse {
+    ErrorResponse::new(Some("Not found."), Some(404))
+}
+
+#[catch(422)]
+fn unprocessable_entity() -> ErrorResponse {
+    ErrorResponse::new(Some("You sent wrong type of data. Check your body."), Some(422))
+}
+
+#[catch(default)]
+fn default_catcher(status: Status, _: &Request) -> ErrorResponse {
+    ErrorResponse::new(Some("Something went wrong."), Some(status.code as usize))
 }
 
 pub fn rocket() -> Rocket<Build> {
@@ -16,5 +30,5 @@ pub fn rocket() -> Rocket<Build> {
         .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
         .mount("/task", routes::task::routes())
         .mount("/progress", routes::progress::routes())
-        .register("/", catchers![not_found])
+        .register("/", catchers![not_found, unprocessable_entity, default_catcher])
 }
